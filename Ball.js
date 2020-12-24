1825056d51dd1d4c6c777ca414a54daf1f8cc34c
vasudhasaini111 <74268691+vasudhasaini111@users.noreply.github.com>
2020-12-24 16:08:34 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,33 @@
+class Ball {
+  constructor(x, y,width,height) {
+    var options = { 
+      restitution:1,
+      density: 1,
+    }
+
+    this.body = Bodies.rectangle(x, y,width,height, options);
+    this.width=width;
+    this.height=height;
+    this.image=loadImage("sprites/METALBALL.png")
+    World.add(world, this.body);
+  }
+
+  display() {
+    var angle = this.body.angle;
+    push();
+    translate(this.body.position.x,this.body.position.y);
+    rotate(angle);
+ 
+    //fill("purple");
+    //ellipseMode(CENTER);
+    imageMode(CENTER);
+    image(this.image,0,0,this.width,this.height);
+    //ellipse(this.body.position.x,this.body.position.y,80,80);
+    pop();
+  }
+}
+
+
+
+     
+          
