b101b4deec6d1a888bf3ddc01431c4f6e13469ce
vasudhasaini111 <74268691+vasudhasaini111@users.noreply.github.com>
2020-12-24 16:21:28 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,29 @@
+class Box {
+  constructor(x, y, width, height) {
+    var options = {
+        'restitution':0.8,
+        'friction':1.0,
+        'density':0.04,
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.image=loadImage("Bricks.png")
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+   // rectMode(CENTER);
+    //strokeWeight(4);
+    //stroke("purple");
+    //fill("pink");
+    imageMode(CENTER);
+    image(this.image,0, 0, this.width, this.height);
+    pop();
+  }
+}
